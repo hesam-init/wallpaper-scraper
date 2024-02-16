0286cf1f9b274125052e37b1c13e25dfacdf5939
hesam-init <hesam.init@gmail.com>
2024-02-16 23:33:41 +0330

    refactor: deno tasks

--- src/cache.ts
+++ src/cache.ts
@@ -3,9 +3,9 @@ clearCache();
 async function clearCache() {
 	try {
 		await Deno.remove("db.sqlite");
-		console.log("cache removed !");
+		console.log("Cache removed !");
 	} catch (err) {
-		console.log("no cache to clear !");
+		console.log("No cache to remove !");
 
 		if (!(err instanceof Deno.errors.NotFound)) {
 			throw err;
